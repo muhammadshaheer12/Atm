117d585ea0e21e037b29d8379abe8db4cbf0861d
MuhammadShaheer12 <159147097+MuhammadShaheer12@users.noreply.github.com>
2024-03-29 04:11:12 +0500

    commit

--- main.js
+++ main.js
@@ -1,41 +1,55 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-let mybalance = 15000; // dollar
-let mypin = 1597;
+let mybalance = 10000;
+let mypin = 1234;
 let pinanswer = await inquirer.prompt([
     {
         name: "pin",
         message: "enter your pin",
-        type: "number"
+        type: "number",
     }
 ]);
-// 12345 === 1597 - false
 if (pinanswer.pin === mypin) {
-    console.log("right pin code!!!");
-    let cashans = await inquirer.prompt([
+    console.log("correct pin code!!!");
+    let operationans = await inquirer.prompt([
         {
-            name: "cash",
-            message: "enter your amount ",
+            name: "operation",
+            message: "please select option ",
             type: "list",
-            choices: ["withdraw", "check balance"]
+            choices: ["withdraw", "check balance", "fast cash"],
         }
     ]);
-    if (cashans.cash === "withdraw") {
+    if (operationans.operation === "withdraw") {
         let amountans = await inquirer.prompt([
             {
                 name: "amount",
                 message: "enter your amount",
-                type: "number"
+                type: "number",
             }
         ]);
-        // =, -=, +=
-        mybalance -= amountans.amount;
-        console.log("your remaining balance is: " + mybalance);
+        if (operationans.operation === "check balance") {
+            console.log(`your balance is: {mybalance}`);
+        }
+        else {
+            console.log("insufficient balance");
+        }
     }
-    else if (cashans.cash === "check balance") {
-        console.log("your balance is: " + mybalance);
+    else if (operationans.operation === "check balance") {
+        console.log(`your remaining amount is: {mybalance}`);
+    }
+    if (operationans.operation === "fast cash") {
+        let fastcashans = await inquirer.prompt([
+            {
+                name: "fast cash",
+                message: "enter your amount ",
+                type: "list",
+                choices: ["2000", "5000", "7000", "9000"],
+            }
+        ]);
+        mybalance -= fastcashans.fastcash;
+        console.log(`your remaining amount is: {mybalance}`);
     }
 }
 else {
-    console.log("wrong pin number");
+    console.log("incorrect pin code");
 }
